52c161b3bbf9b6d465ca1e8100b2a46494241d75
samlan24 <muchemionline@gmail.com>
2024-08-26 10:42:04 +0300

    added oauth

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+import os
+
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:your_new_password@localhost:3306/trial'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
