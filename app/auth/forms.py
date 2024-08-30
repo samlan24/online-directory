da349fcb4e627ecdd3d65a4e67f2bc259ceb0cf2
samlan24 <muchemionline@gmail.com>
2024-08-30 05:28:51 +0300

    adding a registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo
+from wtforms import ValidationError
+from app.models import Agent
 
-
+# form to login
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+# form to register new account
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    name = StringField('name', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                            'Usernames must have only letters, '
+                                                                                            'numbers, dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords not matching')])
+    password2 = PasswordField('password2', validators=[DataRequired()])
+    Submit = SubmitField('Register')
+
+# check if email is in use
+def validate_email(self, field):
+    if Agent.query.filter_by(email=field.data).first():
+        raise ValidationError('Email already registered.')
+
+# check if username is in use
+def validate_name(self, field):
+    if Agent.query.filter_by(name=field.data).first():
+        raise ValidationError('name already in use.')
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, session, request
 from app.models import Agent
+from app import db
 from flask_login import login_user, logout_user, current_user
 from . import auth
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 
+# log in route
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -15,7 +17,7 @@ def login():
         flash('invalid username or password')
     return render_template('login.html', form=form)
 
-
+# logout
 @auth.route('/logout')
 def logout():
     logout_user()
@@ -23,3 +25,16 @@ def logout():
     return redirect(url_for("main.index"))
 
 
+# registering
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = Agent(email=form.email.data,
+                     name=form.name.data,
+                     password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
